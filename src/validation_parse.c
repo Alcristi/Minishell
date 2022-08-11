0c777e351e330be4eb43ef35ea919efd28152726
Allan Cristian <alcristi.sto@gmail.com>
2022-08-11 20:16:11 -0300

    Validações do parse

---
+++ src/validation_parse.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validation_parse.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alcristi <alcrist@student.42sp.org.br>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/02 11:56:47 by alcristi          #+#    #+#             */
+/*   Updated: 2022/08/02 11:56:48 by alcristi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+int	is_valid_input(t_token *cursor)
+{
+	if (!ft_strncmp("<", cursor->str, ft_strlen("<"))
+		&& (cursor->next == NULL || cursor->next->str[0] == '<'
+			|| cursor->next->str[0] == '>' || cursor->next->str[0] == '|'))
+	{
+		if (cursor->next == NULL)
+			printf("bash: syntax error near unexpected token `newline'\n");
+		else
+			printf("bash: syntax error near unexpected token `%s'\n",
+				cursor->next->str);
+		return (0);
+	}
+	return (1);
+}
+
+int	is_valid_out(t_token *cursor)
+{
+	if (!ft_strncmp(">", cursor->str, ft_strlen(">"))
+		&& (cursor->next == NULL || cursor->next->str[0] == '<'
+			|| cursor->next->str[0] == '>' || cursor->next->str[0] == '|'))
+	{
+		if (cursor->next == NULL)
+			printf("bash: syntax error near unexpected token `newline'\n");
+		else
+			printf("bash: syntax error near unexpected token `%s'\n",
+				cursor->next->str);
+		return (0);
+	}
+	return (1);
+}
+
+int	is_valid_heredoc(t_token *cursor)
+{
+	if (!ft_strncmp("<<", cursor->str, ft_strlen("<<"))
+		&& (cursor->next == NULL || cursor->next->str[0] == '<'
+			|| cursor->next->str[0] == '>' || cursor->next->str[0] == '|'))
+	{
+		if (cursor->next == NULL)
+			printf("bash: syntax error near unexpected token `newline'\n");
+		else
+			printf("bash: syntax error near unexpected token `%s'\n",
+				cursor->next->str);
+		return (0);
+	}
+	return (1);
+}
+
+int	is_valid_out_append(t_token *cursor)
+{
+	if (!ft_strncmp(">>", cursor->str, ft_strlen(">>"))
+		&& (cursor->next == NULL || cursor->next->str[0] == '<'
+			|| cursor->next->str[0] == '>' || cursor->next->str[0] == '|'))
+	{
+		if (cursor->next == NULL)
+			printf("bash: syntax error near unexpected token `newline'\n");
+		else
+			printf("bash: syntax error near unexpected token `%s'\n",
+				cursor->next->str);
+		return (0);
+	}
+	return (1);
+}
+
+int	is_valid_pipe(t_token *cursor)
+{
+	if (!ft_strncmp("|", cursor->str, ft_strlen("|"))
+		&& (cursor->next == NULL || cursor->previus == NULL
+			|| cursor->next->str[0] == '<'
+			|| cursor->next->str[0] == '>' || cursor->next->str[0] == '|'))
+	{
+		printf("bash: syntax error near unexpected token `|'\n");
+		return (0);
+	}
+}
