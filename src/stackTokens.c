53d5a888aa7e12968734bdc97c0787eb8e5f4e26
Allan Cristian <alcristi.sto@gmail.com>
2022-08-17 13:23:51 -0300

    Criação de função para carregar fila

--- src/stackTokens.c
+++ src/stackTokens.c
@@ -6,7 +6,7 @@
 /*   By: alcristi <alcrist@student.42sp.org.br>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/14 10:52:42 by alcristi          #+#    #+#             */
-/*   Updated: 2022/08/14 10:54:39 by alcristi         ###   ########.fr       */
+/*   Updated: 2022/08/16 18:14:23 by alcristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,35 @@ void	load_stacks(t_token **node, t_token *data)
 	}
 }
 
+void	load_stacks_last(t_token **node, t_token *data)
+{
+	t_token	*new_node;
+	t_token	*cursor;
+	char	*str;
+
+	str = ft_strdup(data->str);
+	if (!*node)
+	{
+		node[0] = ft_calloc(sizeof(t_token), 1);
+		node[0][0] = *data;
+		node[0][0].str = str;
+		node[0][0].next = NULL;
+		node[0][0].previus = NULL;
+	}
+	else
+	{
+		new_node = ft_calloc(sizeof(t_token), 1);
+		*new_node = *data;
+		cursor = node[0];
+		while(cursor->next)
+			cursor = cursor->next;
+		new_node->str = str;
+		new_node->previus = cursor;
+		new_node->next = NULL;
+		cursor->next = new_node;
+	}
+}
+
 t_stacks	*build_stack(t_token *tokens)
 {
 	t_stacks	*new;
@@ -56,7 +85,7 @@ t_stacks	*build_stack(t_token *tokens)
 		else if (cursor->is_heredoc && cursor->str[0] != '<')
 			load_stacks(&new->stack_herodoc, cursor);
 		else if (cursor->is_cmd || cursor->is_arg || cursor->is_pipe)
-			load_stacks(&new->stack_cmd, cursor);
+			load_stacks_last(&new->stack_cmd, cursor);
 		cursor = cursor->next;
 	}
 	return (new);
