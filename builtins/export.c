f670ef036ab9e7ae6fa2b4655ea998c052e0c156
esilva-s <esilva-s@student.42sp.org.br>
2022-09-21 22:50:36 +0200

    adição da mensagem de erro e da verificação do nome da variavel na export

--- builtins/export.c
+++ builtins/export.c
@@ -6,11 +6,29 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 19:48:41 by esilva-s          #+#    #+#             */
-/*   Updated: 2022/09/21 02:23:23 by esilva-s         ###   ########.fr       */
+/*   Updated: 2022/09/21 22:48:48 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+static int	verify_name(char *name)
+{
+	int	count;
+
+	if (name == NULL)
+		return (1);
+	count = 0;
+	if (!ft_isalpha(name[count]))
+		return (1);
+	while (name[count] != '\0')
+	{
+		if (!ft_isalnum(name[count]))
+			return (1);
+		count++;
+	}
+	return (0);
+}
 
 static char	*catch_name(char *arg)
 {
@@ -25,6 +43,8 @@ static char	*catch_name(char *arg)
 		pos_equal++;
 	if (pos_equal >= ft_strlen(arg))
 		return (NULL);
+	if (pos_equal == 0)
+		return (NULL);
 	name = malloc(sizeof(char) * pos_equal);
 	while (count < pos_equal)
 	{
@@ -35,7 +55,7 @@ static char	*catch_name(char *arg)
 	return (name);
 }
 
-
+/*
 static char	*catch_content(char *arg)
 {
 	char	*content;
@@ -55,8 +75,7 @@ static char	*catch_content(char *arg)
 	}
 	content[count] = '\0';
 	return (content);
-}
-
+}*/
 
 static void	print_vars(void)
 {
@@ -83,7 +102,6 @@ int	localize_var(char *name_var, char *arg)
 	{
 		if (search_var(name_var, aux_env->data))
 		{
-			printf("DentroDentro\n");
 			free(aux_env->data);
 			aux_env->data = ft_strdup(arg);
 			return (1);
@@ -104,15 +122,16 @@ int	bt_export(char *arg)
 		return (0);
 	}
 	name = catch_name(arg);
-	if (name == NULL)
+	if (name == NULL || verify_name(name))
+	{
+		printf("`%s\': not a valid identifier\n", arg);
 		return (1);
+	}
 	if (localize_var(name, arg))
 	{
-		printf("dentro\n");
 		free(name);
 		return (0);
 	}
-	printf("fora\n");
 	add_node_last(&g_core_var->env, arg);
 	free(name);
 	return (0);
