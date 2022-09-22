b35d39557cf3f07d4cadb2a24db12e9435c6e9b9
esilva-s <esilva-s@student.42sp.org.br>
2022-09-22 22:45:21 +0200

    fix erro na exibição dos diretorios no pwd

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,75 +6,20 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 23:30:03 by esilva-s          #+#    #+#             */
-/*   Updated: 2022/08/22 23:14:42 by esilva-s         ###   ########.fr       */
+/*   Updated: 2022/09/22 22:44:43 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static int	is_pwd(char *str)
-{
-	int	count;
-	int	size;
-
-	count = 0;
-	size = ft_strlen(str);
-	if (size < 4)
-		return (0);
-	if (str[0] == 'P')
-		count++;
-	if (str[1] == 'W')
-		count++;
-	if (str[2] == 'D')
-		count++;
-	if (str[3] == '=')
-		count++;
-	if (count == 4)
-		return (1);
-	else
-		return (0);
-}
-
-static char	*catch_pwd(char *data)
-{
-	int		size;
-	int		count;
-	char	*result;
-
-	count = 0;
-	size = ft_strlen(data);
-	result = NULL;
-	result = malloc(sizeof(char *) * size - 3);
-	while (count + 4 < size)
-	{
-		result[count] = data[count + 4];
-		count++;
-	}
-	result[count] = '\0';
-	return (result);
-}
-
 int	bt_pwd(void)
 {
-	t_double_list	*temp;
-	char			*result;
+	char	*path_current;
 
-	result = NULL;
-	temp = g_core_var->env;
-	while (temp != NULL)
-	{
-		if (is_pwd(temp->data))
-		{
-			result = catch_pwd(temp->data);
-			if (result != NULL)
-			{
-				printf("%s\n", result);
-				free(result);
-				return (0);
-			}
-		}
-		else
-			temp = temp->next;
-	}
+	path_current = NULL;
+	path_current = getcwd(NULL, 0);
+	printf("%s\n", path_current);
+	if (path_current != NULL)
+		free(path_current);
 	return (1);
 }
