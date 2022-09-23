0eab63b9947f24920edfc42a0361635626bf6b20
Allan Cristian <alcristi.sto@gmail.com>
2022-09-23 07:58:03 -0300

    modificações

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: alcristi <alcrist@student.42sp.org.br>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 22:38:41 by esilva-s          #+#    #+#             */
-/*   Updated: 2022/09/20 17:54:44 by alcristi         ###   ########.fr       */
+/*   Updated: 2022/09/22 22:31:49 by alcristi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@ int	bt_echo(int nb, char **args)
 	int	count;
 	int	break_line;
 
-	if (args == NULL)
-		return (1);
 	count = 0;
 	break_line = 1;
-	while (count < nb)
+	while (count < nb && args)
 	{
 		if (!ft_strcmp(args[count], "-n"))
 			break_line = 0;
@@ -29,7 +27,7 @@ int	bt_echo(int nb, char **args)
 			printf("%s ", args[count]);
 		count++;
 	}
-	if (break_line == 1)
+	if (break_line == 1 || args == NULL)
 		printf("\n");
 	return (0);
 }
