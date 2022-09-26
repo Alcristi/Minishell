23243417596a28fa26ff19791f4dbdea1ab9115d
esilva-s <esilva-s@student.42sp.org.br>
2022-09-26 21:47:26 +0200

    fix erro de alocacao cd

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: esilva-s <esilva-s@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 02:24:26 by esilva-s          #+#    #+#             */
-/*   Updated: 2022/09/22 19:19:03 by esilva-s         ###   ########.fr       */
+/*   Updated: 2022/09/26 21:46:54 by esilva-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	bt_cd(char *path)
 {
 	int		result;
+	char	*aux;
 
 	if (path == NULL)
 	{
@@ -28,11 +29,6 @@ int	bt_cd(char *path)
 	{
 		printf("cd: %s: No such file or directory\n", path);
 		return (-1);
-	}
-	else
-	{
-		//free_dir(g_core_var);
-		init_dir(&g_core_var->prompt);
 	}
 	return (0);
 }
